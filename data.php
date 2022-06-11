70779a4a4a4edf7905a600e2e9130f9ab502db75
Velzewnul <vitbulgakov@gmail.com>
2022-06-11 03:31:06 +0300

    Добавил лотам срок истечения (правда работает не очень правильно)

--- data.php
+++ data.php
@@ -12,37 +12,43 @@ $goods = [
         "title" => "2014 Rossignol District Snowboard",
         "category" => "Доски и лыжи",
         "price" => "10999",
-        "url" => "img/lot-1.jpg"
+        "url" => "img/lot-1.jpg",
+        "expiration_date" => "2022-09-11"
     ],
     [
         "title" => "DC Ply Mens 2016/2017 Snowboard",
         "category" => "Доски и лыжи",
         "price" => "159999",
-        "url" => "img/lot-2.jpg"
+        "url" => "img/lot-2.jpg",
+        "expiration_date" => "2022-07-11"
     ],
     [
         "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
         "category" => "Крепления",
         "price" => "8000",
-        "url" => "img/lot-3.jpg"
+        "url" => "img/lot-3.jpg",
+        "expiration_date" => "2022-08-25"
     ],
     [
         "title" => "Ботинки для сноуборда DC Mutiny Charocal",
         "category" => "Ботинки",
         "price" => "10999",
-        "url" => "img/lot-4.jpg"
+        "url" => "img/lot-4.jpg",
+        "expiration_date" => "2022-07-17"
     ],
     [
         "title" => "Куртка для сноуборда DC Mutiny Charocal",
         "category" => "Одежда",
         "price" => "7500",
-        "url" => "img/lot-5.jpg"
+        "url" => "img/lot-5.jpg",
+        "expiration_date" => "2023-01-11"
     ],
     [
         "title" => "Маска Oakley Canopy",
         "category" => "Разное",
         "price" => "5400",
-        "url" => "img/lot-6.jpg"
+        "url" => "img/lot-6.jpg",
+        "expiration_date" => "2022-10-12"
     ]
 ];
 ?>
--- functions.php
+++ functions.php
@@ -9,4 +9,25 @@
             $price=number_format($price, "0", "", " ");
             return "$price Р";
         }
-?>
+/**
+ * Подсчитывает оставшееся для лота время аукциона
+ * @param integer $date Дата окончания
+ * @return array
+ */
+    function get_time_left($date) {
+        date_default_timezone_set('Europe/Moscow');
+        $final_date = date_create($date);
+        $cur_date = date_create("now");
+        $diff = date_diff($final_date, $cur_date);
+        $format_diff = date_interval_format($diff, "%d %H %I");
+        $arr = explode(" ", $format_diff);
+
+        $hours = $arr[0] * 24 + $arr[1];
+        $minutes = intval($arr[2]);
+        $hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+        $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+        $res[] = $hours;
+        $res[] = $minutes;
+
+        return $res;
+    }
