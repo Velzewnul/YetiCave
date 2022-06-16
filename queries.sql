daa382e48aa9ae7c832deb412830079dfd70fc6f
Velzewnul <vitbulgakov@gmail.com>
2022-06-16 21:00:59 +0300

    Добавил файл queries для заполнения БД UPD: заполнил категории и сделал шаблоны под заполнение пользователей, лотов и ставок.

---
+++ queries.sql
@@ -0,0 +1,16 @@
+INSERT INTO YetiCave.categories(category_name, symbolic_name)
+VALUES ('Доски и лыжи', 'boards'),
+       ('Крепления', 'attachment'),
+       ('Ботинки', 'boots'),
+       ('Одежда', 'clothing'),
+       ('Инструменты', 'tools'),
+       ('Разное', 'other');
+
+INSERT INTO YetiCave.users(id, registration_date, email, name, password, contact_info)
+VALUES ();
+
+INSERT INTO YetiCave.lots(id, add_date, lot_title, lot_description, lot_image, start_price, end_price, bet_step)
+VALUES ();
+
+INSERT INTO YetiCave.bets(bet_date, bet_sum)
+VALUES ();
