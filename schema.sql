79be4b524e4133e70ec9a469169558d641c325d8
Velzewnul <vitbulgakov@gmail.com>
2022-06-16 20:32:30 +0300

    Добавил файл схемы базы данных fix: добавил индексы

--- schema.sql
+++ schema.sql
@@ -7,32 +7,32 @@ USE YetiCave;
 CREATE TABLE YetiCave.categories
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name char(64),
-    symbolic_name char(64)
+    category_name char(64) NOT NULL UNIQUE,
+    symbolic_name char(64) NOT NULL UNIQUE
 );
 CREATE TABLE YetiCave.lots
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    lot_title CHAR(64),
+    lot_title CHAR(64) NOT NULL,
     lot_description TEXT,
-    lot_image CHAR(64),
-    start_price int,
-    end_price int,
-    bet_step int
+    lot_image CHAR(64) NOT NULL,
+    start_price int NOT NULL,
+    end_price int NOT NULL,
+    bet_step int NOT NULL
 );
 CREATE TABLE YetiCave.bets
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     bet_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    bet_sum int
+    bet_sum int NOT NULL
 );
 CREATE TABLE YetiCave.users
 (
     id                INT AUTO_INCREMENT PRIMARY KEY,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email             char(64),
-    name              char(10),
-    password          char(12),
-    contact_info      char(128)
+    email             char(64) NOT NULL UNIQUE,
+    name              char(10) NOT NULL,
+    password          char(12) NOT NULL,
+    contact_info      char(128) NOT NULL
 );
