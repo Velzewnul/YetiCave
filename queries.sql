2b32cb6f9cefd506fef611b28fa2d5e7ca23cd54
Velzewnul <vitbulgakov@gmail.com>
2022-06-17 01:26:03 +0300

    Добавил файл queries для заполнения БД Исправил файл schem.sql из прошлого задания, добавив FOREIGN KEYS. В queries добавил товары и пользователей

--- queries.sql
+++ queries.sql
@@ -6,11 +6,18 @@ VALUES ('Доски и лыжи', 'boards'),
        ('Инструменты', 'tools'),
        ('Разное', 'other');
 
-INSERT INTO YetiCave.users(id, registration_date, email, name, password, contact_info)
-VALUES ();
+INSERT INTO YetiCave.users(registration_date, email, name, password, contact_info)
+    VALUES ('27.08.1996','oslan@mail.ru', 'Nekitos', '123456', 'Zvonite na mobilu vsegda otve4u'),
+           ('16.03.1998','nepishi@gmail.com', 'Unknown', '123456', 'Menya tut net')
+;
 
-INSERT INTO YetiCave.lots(id, add_date, lot_title, lot_description, lot_image, start_price, end_price, bet_step)
-VALUES ();
+INSERT INTO YetiCave.lots(lot_title, lot_image, start_price, end_date, bet_step, user_id, category_id)
+VALUES ('2014 Rossignol District Snowboard','img/lot-1.jpg', 10999, '2022-09-11', 600, 1, 1),
+       ('DC Ply Mens 2016/2017 Snowboard', 'img/lot-2.jpg', 159999, '2022-07-11', 1000, 1, 1),
+       ('Крепления Union Contact Pro 2015 года размер L/XL', 'img/lot-3.jpg', 8000, '2022-08-25', 300, 2, 2),
+       ('Ботинки для сноуборда DC Mutiny Charocal', 'img/lot-4.jpg', 10999, '2022-07-17', 500, 2, 3),
+       ('Куртка для сноуборда DC Mutiny Charocal', 'img/lot-5.jpg', 7500, '2023-01-11', 450, 2, 4),
+       ('Маска Oakley Canopy', 'img/lot-6.jpg', 5400, '2022-10-12', 700, 2, 6)
+       ;
 
-INSERT INTO YetiCave.bets(bet_date, bet_sum)
-VALUES ();
+
--- schema.sql
+++ schema.sql
@@ -4,35 +4,51 @@ default character set utf8
 default collate utf8_general_ci;
 USE YetiCave;
 
-CREATE TABLE YetiCave.categories
+CREATE TABLE categories
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name char(64) NOT NULL UNIQUE,
     symbolic_name char(64) NOT NULL UNIQUE
 );
-CREATE TABLE YetiCave.lots
+
+CREATE TABLE lots
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    lot_title CHAR(64) NOT NULL,
+    lot_title VARCHAR(64) NOT NULL,
     lot_description TEXT,
-    lot_image CHAR(64) NOT NULL,
+    lot_image VARCHAR(64),
     start_price int NOT NULL,
-    end_price int NOT NULL,
-    bet_step int NOT NULL
-);
-CREATE TABLE YetiCave.bets
+    end_date DATE,
+    bet_step int,
+    user_id int,
+    winner_id int,
+    category_id int,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (winner_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id);
+
+CREATE TABLE bets
 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     bet_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     bet_sum int NOT NULL
+    user_id int,
+    lot_id int,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lot_id) REFERENCES lots(id)
 );
-CREATE TABLE YetiCave.users
+
+CREATE TABLE users
 (
     id                INT AUTO_INCREMENT PRIMARY KEY,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email             char(64) NOT NULL UNIQUE,
     name              char(10) NOT NULL,
-    password          char(12) NOT NULL,
-    contact_info      char(128) NOT NULL
+    password          VARCHAR(12) NOT NULL,
+    contact_info      char(128) NOT NULL,
+    lot_id int,
+    bet_id int,
+    FOREIGN KEY (lot_id) REFERENCES lots(id),
+    FOREIGN KEY (bet_id) REFERENCES bets(id)
 );
